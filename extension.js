8264a845c915e0ceaea7ac8cb24218eee292b0bb
Ashesh Kumar Singh <user501254@gmail.com>
2019-05-09 20:53:43 +0530

    Add indicator button  and license file
    see:
      https://github.com/GNOME/gnome-shell/blob/gnome-3-32/js/ui/status/accessibility.js
      https://gitlab.gnome.org/GNOME/gnome-shell-extensions/blob/gnome-3-32/extensions/drive-menu/extension.js

--- extension.js
+++ extension.js
@@ -1,57 +1,35 @@
 
-const St = imports.gi.St;
+const { GObject, St } = imports.gi;
 const Main = imports.ui.main;
-const Tweener = imports.ui.tweener;
 
-let text, button;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
 
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
-}
-
-function _showHello() {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
-        Main.uiGroup.add_actor(text);
+var touchpadIndicatorButton = GObject.registerClass(
+    class TouchpadIndicatorButton extends PanelMenu.Button {
+        _init() {
+            super._init(0.0, 'Touchpad Indicator');
+            let hbox = new St.BoxLayout({
+                style_class: 'panel-status-menu-box'
+            });
+            let icon = new St.Icon({
+                icon_name: 'input-touchpad-symbolic',
+                style_class: 'system-status-icon'
+            });
+            hbox.add_child(icon);
+            hbox.add_child(PopupMenu.arrowIcon(St.Side.BOTTOM));
+            this.add_child(hbox);
+            this.actor.show();
+        }
     }
-
-    text.opacity = 255;
-
-    let monitor = Main.layoutManager.primaryMonitor;
-
-    text.set_position(monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
-        monitor.y + Math.floor(monitor.height / 2 - text.height / 2));
-
-    Tweener.addTween(text,
-        {
-            opacity: 0,
-            time: 2,
-            transition: 'easeOutQuad',
-            onComplete: _hideHello
-        });
-}
+);
 
 function init() {
-    button = new St.Bin({
-        style_class: 'panel-button',
-        reactive: true,
-        can_focus: true,
-        x_fill: true,
-        y_fill: false,
-        track_hover: true
-    });
-    let icon = new St.Icon({
-        icon_name: 'system-run-symbolic',
-        style_class: 'system-status-icon'
-    });
-
-    button.set_child(icon);
-    button.connect('button-press-event', _showHello);
 }
 
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+    _indicator = new touchpadIndicatorButton;
+    Main.panel.addToStatusArea('touchpad-indicator', _indicator);
 }
 
 function disable() {
