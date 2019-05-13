1cab2ae41338a394cd728fe0dbcfca78a574b073
Ashesh Kumar Singh <user501254@gmail.com>
2019-05-14 00:01:55 +0530

    Add extension helper files
    Adding as it is right now. Need to refactor.

---
+++ lib.js
@@ -0,0 +1,188 @@
+/* TouchpadIndicator - Touchpad management GNOME Shell Extension.
+ * Orignal work Copyright (C) 2011-2013 Armin Köhler <orangeshirt at web.de>
+ * Modifcations Copyright (C) 2019 Ashesh Singh <user501254 at gmail.com>
+ *
+ * This file is part of TouchpadIndicator, a fork of Armin Köhler's
+ * 'gnome-shell-extension-touchpad-indicator' project which is licensed GPLv2.
+ * Orignal source code is available at https://git.io/fjVec.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to:
+ * The Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA 02110-1301, USA.
+ */
+
+
+const { Gio, GLib } = imports.gi;
+const ByteArray = imports.byteArray;
+
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+
+// Settings
+const SCHEMA_EXTENSION = 'org.gnome.shell.extensions.touchpad-indicator';
+const SCHEMA_TOUCHPAD = 'org.gnome.desktop.peripherals.touchpad';
+
+// Debug Mode Settings
+const LOG_FILEPATH = GLib.build_filenamev([Me.path, 'touchpad-indicator.log']);
+const LOG_PREFIX = `[${Me.uuid}]` + ': ';
+var DEBUG = false;
+var DEBUG_TO_FILE = false;
+
+// Possible Devices
+const TOUCHPADS = ['touchpad', 'glidepoint', 'fingersensingpad', 'bcm5974', 'trackpad', 'smartpad'];
+const TRACKPOINTS = ['trackpoint', 'accu point', 'trackstick', 'touchstyk', 'pointing stick', 'dualpoint stick'];
+const TOUCHSCREENS = ['touchscreen', 'maxtouch', 'touch digitizer', 'touch system'];
+const FINGERTOUCHES = ['finger touch'];
+const PENS = ['pen stylus', 'pen eraser'];
+const OTHERS = [];
+const ALL_TYPES = {
+    'touchpad': TOUCHPADS,
+    'trackpoint': TRACKPOINTS,
+    'touchscreen': TOUCHSCREENS,
+    'fingertouch': FINGERTOUCHES,
+    'pen': PENS,
+    'other': OTHERS
+};
+var ALL_TOUCHPADS = TOUCHPADS.slice();
+var ALL_OTHERS = OTHERS.slice();
+
+// Methods to enable or disable the touchpad
+const METHOD = { GCONF: 0, SYNCLIENT: 1, XINPUT: 2 };
+
+
+function createLogFile(filepath) {
+    const PERMISSIONS_MODE = 0o755;
+    let isSuccess = false;
+    filepath = (filepath !== undefined) ? filepath : LOG_FILEPATH;
+    try {
+        let file = Gio.File.new_for_path(filepath);
+        if (GLib.mkdir_with_parents(file.get_parent().get_path(), PERMISSIONS_MODE) === 0) {
+            isSuccess = file.replace_contents('', null, false,
+                Gio.FileCreateFlags.REPLACE_DESTINATION, null)[0];
+        }
+        return isSuccess;
+    } catch (err) {
+        let error = `Sorry could not create logfile!\n${err}`;
+        return [isSuccess, error];
+    }
+
+}
+
+function writeLog(filepath, contents) {
+    let isSuccess = false;
+    try {
+        isSuccess = GLib.file_set_contents(filepath, contents);
+        return isSuccess;
+    } catch (err) {
+        let error = `Sorry could not write to logfile!\n${err}`;
+        return [isSuccess, error];
+    }
+}
+
+function readLog(filepath) {
+    let [isSuccess, contents] = [false, ''];
+    try {
+        [isSuccess, contents] = GLib.file_get_contents(filepath);
+        return [isSuccess, ByteArray.toString(contents)];
+    } catch (err) {
+        let error = `Sorry could not read from logfile!\n${err}`;
+        return [isSuccess, error];
+    }
+}
+
+function logger(event, filepath) {
+    let timestamp = new Date(new Date().getTime()).toISOString();
+    let message = `${timestamp}    ${event}\n`;
+    global.log(LOG_PREFIX + message);
+    if (filepath) {
+        let messages = readLog(filepath)[1] + message;
+        writeLog(filepath, messages);
+    }
+}
+
+
+function executeCmdAsync(command) {
+    try {
+        return GLib.spawn_command_line_async(command);
+    } catch (err) {
+        logger(err.message.toString());
+        return false;
+    }
+}
+
+function executeCmdSync(command) {
+    let [isSuccess, stdOut] = [false, ''];
+    try {
+        [isSuccess, stdOut] = GLib.spawn_command_line_sync(command);
+        return [isSuccess, ByteArray.toString(stdOut)];
+    } catch (err) {
+        logger(err.message.toString());
+        return false;
+    }
+}
+
+function watchDevInput() {
+    let file = Gio.file_new_for_path('/dev/input');
+    return file.monitor_directory(Gio.FileMonitorFlags.WATCH_MOUNTS, null);
+}
+
+
+function makePointingDevice(pointingDeviceLines) {
+    //assuming that N: & P: always appear at lines 2 and 3 respectively
+    if (pointingDeviceLines[1].startsWith('N: Name=') &&
+        pointingDeviceLines[2].startsWith('P: Phys=')) {
+        let pointingDevice = {};
+        pointingDevice.name = pointingDeviceLines[1].split('"')[1];
+        pointingDevice.phys = pointingDeviceLines[2].split('=')[1];
+        pointingDevice.type = 'other/mouse';
+        for (let type in ALL_TYPES) {
+            if (ALL_TYPES[type].some((v) => {
+                return (pointingDevice.name.toLowerCase().indexOf(v) >= 0);
+            })) {
+                pointingDevice.type = type;
+                break;
+            }
+        }
+        return pointingDevice;
+    }
+}
+
+function listPointingDevices() {
+    let pointingDevices = [];
+    let comp = executeCmdSync('cat /proc/bus/input/devices');
+    let allDeviceChunks = comp[1].split('\n\n');
+    for (let x = 0; x < allDeviceChunks.length; x++) {
+        if (!(allDeviceChunks[x].indexOf('mouse') === -1)) {
+            let pointingDeviceLines = allDeviceChunks[x].split('\n');
+            let pointingDevice = makePointingDevice(pointingDeviceLines);
+            if (pointingDevice !== undefined) {
+                pointingDevices.push(pointingDevice);
+            }
+        }
+    }
+    if (pointingDevices[0]) {
+        return [true, pointingDevices];
+    } else {
+        return [false, '    - No Pointing Devices detected.\n'];
+    }
+}
+
+
+function removeSource(...args) {
+    return GLib.source_remove(...args);
+}
+
+function addTimeout(...args) {
+    return GLib.timeout_add(...args);
+}
---
+++ xinput.js
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2011-2013 Armin Köhler <orangeshirt at web.de>
+ *
+ * Thanks to Lorenzo Carbonell Cerezo and Miguel Angel Santamaría Rogado
+ * which has written touchpad-indicator
+ * (https://launchpad.net/touchpad-indicator) as python app and inspired
+ * myself to write this extension for gnome-shell.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to:
+ * The Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA 02110-1301, USA.
+ */
+
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Lib = Me.imports.lib;
+
+// for consistency
+var USE_XINPUT = true;
+
+let logging = Lib.logger;
+
+function XInput(devices) {
+    this._init(devices);
+}
+
+XInput.prototype = {
+    _init: function (devices) {
+        logging(`XInput._init(${devices})`);
+        this.devices = devices;
+        this.ids = this._getIds();
+        this.isPresent = this._isPresent();
+        logging(`XInput._init(): Found Device - ${
+            this.isPresent.toString()} ${this.ids}`);
+    },
+
+    _getIds: function () {
+        let tpids = [];
+        let y = 0;
+        let allIds = this._getAllIds();
+        for (let id = 0; id < allIds.length; id++) {
+            if (this._isDevice(allIds[id]) === true) {
+                tpids[y] = allIds[id];
+                y++;
+            }
+        }
+        return tpids;
+    },
+
+    _getAllIds: function () {
+        let devids = [];
+        let comp = Lib.executeCmdSync('xinput --list');
+        if (comp[0]) {
+            let lines = comp[1].split('\n');
+            let line = 0;
+            //assuming that 'pointer' lines always appear fist & together
+            while (lines[line].indexOf('pointer') !== -1) {
+                devids.push(lines[line].split('id=')[1].split('\t')[0]);
+                line++;
+            }
+        }
+        return devids;
+    },
+
+    _isDevice: function (id) {
+        let comp = Lib.executeCmdSync(`xinput --list-props ${id.toString()}`);
+        return this._searchDevice(comp[1]);
+    },
+
+    _isPresent: function () {
+        return this.ids.length > 0;
+    },
+
+    _searchDevice: function (where) {
+        if (where) {
+            where = where.toLowerCase();
+            for (let tpid = 0; tpid < this.devices.length; tpid++) {
+                if (!(where.indexOf(
+                    this.devices[tpid].toString().toLowerCase()) === -1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    },
+
+    _setDeviceEnabled: function (id) {
+        logging(`XInput._setDeviceEnabled() id: ${id.toString()}`);
+        return Lib.executeCmdAsync(`xinput set-prop ${id.toString()
+        } "Device Enabled" 1`);
+    },
+
+    _setDeviceDisabled: function (id) {
+        logging(`XInput._setDeviceDisabled() id: ${id.toString()}`);
+        return Lib.executeCmdAsync(`xinput set-prop ${id.toString()
+        } "Device Enabled" 0`);
+    },
+
+    _disableAllDevices: function () {
+        for (let id = 0; id < this.ids.length; id++) {
+            this._setDeviceDisabled(this.ids[id]);
+        }
+        return !this._allDevicesEnabled();
+    },
+
+    _enableAllDevices: function () {
+        for (let id = 0; id < this.ids.length; id++) {
+            this._setDeviceEnabled(this.ids[id]);
+        }
+        return this._allDevicesEnabled();
+    },
+
+    _switchAllDevices: function (state) {
+        for (let id = 0; id < this.ids.length; id++) {
+            if (state) {
+                this._setDeviceEnabled(this.ids[id]);
+            } else {
+                this._setDeviceDisabled(this.ids[id]);
+            }
+        }
+        return this._allDevicesEnabled();
+    },
+
+    _isDeviceEnabled: function (id) {
+        logging('XInput._isDeviceEnabled()');
+        let lines = Lib.executeCmdSync(`xinput --list-props ${id.toString()}`);
+        if (lines) {
+            lines = lines[1].split('\n');
+            for (let line = 0; line < lines.length; line++) {
+                if (lines[line].toString().toLowerCase().indexOf(
+                    'device enabled') !== -1) {
+                    if (lines[line].toString().split(':')[1].indexOf('1')
+                        !== -1) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    },
+
+    _allDevicesEnabled: function () {
+        if (!this.isPresent) {
+            return false;
+        }
+        for (let id = 0; id < this.ids.length; id++) {
+            if (this._isDeviceEnabled(this.ids[id]) === false) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
