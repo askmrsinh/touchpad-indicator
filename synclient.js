28acffa8ce6eefc07571f5b9a31eb40223128ec3
Ashesh Kumar Singh <user501254@gmail.com>
2019-06-12 02:45:12 +0530

    Remove watcher on synclient options

--- synclient.js
+++ synclient.js
@@ -39,9 +39,7 @@ class Synclient {
     _init(gsettings) {
         logging('Synclient._init()');
         this.gsettings = gsettings;
-        this.synclient_status = false;
-        this.stop = false;
-        this.watch = false;
+        this.synclientStatus = false;
         this.timeout = false;
         this.synclientInUse = this._isSynclientInUse();
     }
@@ -81,69 +79,10 @@ class Synclient {
         return false;
     }
 
-    _isSynclientStillInUse() {
-        this.synclientInUse = this._isSynclientInUse();
-        return this.synclientInUse;
-    }
-
-    _watch() {
-        if (!this.stop && !this.wait) {
-            this.output = Lib.executeCmdSync('synclient -l');
-            if (this.output) {
-                let lines = this.output[1].toString().split('\n');
-                for (let x = 0; x < lines.length; x++) {
-                    if (lines[x].includes('TouchpadOff')) {
-                        this.touchpad_off = lines[x];
-                        break;
-                    }
-                }
-                if (!this.synclient_status)
-                    this.synclient_status = this.touchpad_off;
-                if (this.synclient_status === this.touchpad_off) {
-                    this._wait();
-                } else {
-                    let parts = this.touchpad_off.split('= ');
-                    let state = true;
-                    if (parts[1] === '1') {
-                        state = false;
-                    }
-                    logging(`Synclient._watch: Touchpad state changed to ${
-                        state.toString()}`);
-                    this.gsettings.set_boolean('touchpad-enabled', state);
-                    this.synclient_status = this.touchpad_off;
-                    this._wait();
-                }
-            }
-        }
-    }
-
-    _callWatch() {
-        this.wait = false;
-        this._watch();
-    }
-
-    _wait() {
-        this.wait = true;
-        this.timeout = Lib.addTimeout(1000, this._callWatch.bind(this));
-    }
-
-    _cancel() {
-        logging('Synclient._cancel()');
-        this.stop = true;
-        this.wait = false;
-        this.synclient_status = false;
-        if (this.timeout) {
-            Lib.removeSource(this.timeout);
-            this.timeout = false;
-        }
-    }
-
     _disable() {
         logging('Synclient._disable()');
-        this._cancel();
         if (Lib.executeCmdAsync('synclient TouchpadOff=1')) {
-            this.stop = false;
-            this._watch();
+            this.synclientStatus = false;
             return false;
         } else
             return true;
@@ -151,10 +90,8 @@ class Synclient {
 
     _enable() {
         logging('Synclient._enable()');
-        this._cancel();
         if (Lib.executeCmdAsync('synclient TouchpadOff=0')) {
-            this.stop = false;
-            this._watch();
+            this.synclientStatus = false;
             return true;
         } else
             return false;
