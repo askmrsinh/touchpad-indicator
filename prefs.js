7caa7844bf4a195d73d74b4fc92db34bf12f35a1
Ashesh Kumar Singh <user501254@gmail.com>
2019-06-16 13:07:49 +0530

    Add Settings UI
    see:
    https://github.com/micheleg/dash-to-dock/blob/master/prefs.js

--- prefs.js
+++ prefs.js
@@ -1,1 +1,45 @@
-//TODO
+const { Gtk } = imports.gi;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+
+const Me = ExtensionUtils.getCurrentExtension();
+
+// Settings
+const SCHEMA_EXTENSION = 'org.gnome.shell.extensions.touchpad-indicator';
+const SCHEMA_TOUCHPAD = 'org.gnome.desktop.peripherals.touchpad';
+
+var Settings = class TouchpadIndicatorSettings {
+    constructor() {
+        this._settings = ExtensionUtils.getSettings(SCHEMA_EXTENSION);
+
+        this._rtl = (Gtk.Widget.get_default_direction() === Gtk.TextDirection.RTL);
+
+        this._builder = new Gtk.Builder();
+        //this._builder.set_translation_domain(Me.metadata['gettext-domain']);
+        this._builder.add_from_file(`${Me.path}/Settings.ui`);
+
+        this.widget = new Gtk.ScrolledWindow({
+            hscrollbar_policy: Gtk.PolicyType.NEVER,
+            vscrollbar_policy: Gtk.PolicyType.NEVER
+        });
+        this._notebook = this._builder.get_object('ti_notebook');
+        this.widget.add(this._notebook);
+
+        // Set a reasonable initial window height
+        this.widget.connect('realize', () => {
+            let window = this.widget.get_toplevel();
+            window.resize(600, 550);
+        });
+    }
+};
+
+function init() {
+    //ExtensionUtils.initTranslations();
+}
+
+function buildPrefsWidget() {
+    let settings = new Settings();
+    let widget = settings.widget;
+    widget.show_all();
+    return widget;
+}
