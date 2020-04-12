askmrsinh/touchpad-indicator

commit 5a20ebd7ab11461d35f0bab0567e69c04d1a9008

Add logging for discovered pointing devices

extension.js
@@ -107,6 +107,11 @@ class TouchpadIndicatorButton extends PanelMenu.Button {
             this._logSKeyChange.bind(this));
 
         logging('_init()');
+
+        // Retrive all pointing device handlers
+        this.pointingDevices = Lib.listPointingDevices()[1];
+        logging(`_init(): Pointing devices are ${JSON.stringify(this.pointingDevices)}`);
+
         // TODO: Let user set program start touchpad state
         //       None, Enabled, Disabled
 
@@ -539,10 +544,9 @@ class TouchpadIndicatorButton extends PanelMenu.Button {
         // TODO: Consider autoswitch-* key was set to 'false' while touchpad
         //       is disabled and then user unplugs the mouse.
         if (this._extSettings.get_boolean('autoswitch-touchpad')) {
-            let pointingDevices = Lib.listPointingDevices()[1];
-            let mouseDevices = pointingDevices.filter(p => p.type === 'mouse');
+            let mouseDevices = this.pointingDevices.filter(p => p.type === 'mouse');
             let mouseCount = mouseDevices.length;
-
+            logging(`_onMouseDevicePlugged(${eventType}) - mouseDevices are ${JSON.stringify(mouseDevices)}`);
             logging(`_onMouseDevicePlugged(${eventType}) - mouseCount is ${mouseCount}`);
 
             // no mouse device(s) is/are plugged in

lib.js
@@ -157,6 +157,7 @@ function makePointingDevice(pointingDeviceLines) {
                 break;
             }
         }
+        logger(JSON.stringify(pointingDevice));
         return pointingDevice;
     }
 }

commit cd882ca0c8e5512e58489784d8535fec7d65f425

Fix autoswitch regression
This fixes a autoswitch regression introduced in commit 5a20ebd7

extension.js
@@ -110,7 +110,6 @@ class TouchpadIndicatorButton extends PanelMenu.Button {
 
         // Retrive all pointing device handlers
         this.pointingDevices = Lib.listPointingDevices()[1];
-        logging(`_init(): Pointing devices are ${JSON.stringify(this.pointingDevices)}`);
 
         // Current session type
         // You may face issues using this extension under Wayland.
@@ -545,6 +544,9 @@ class TouchpadIndicatorButton extends PanelMenu.Button {
 
     _onMouseDevicePlugged(eventType) {
         logging(`_onMouseDevicePlugged(${eventType})`);
+
+        // Retrive all pointing device handlers
+        this.pointingDevices = Lib.listPointingDevices()[1];
 
         // TODO: Check auto switch behaviour on resume from sleep, restart.
         // TODO: Consider autoswitch-* key was set to 'false' while touchpad